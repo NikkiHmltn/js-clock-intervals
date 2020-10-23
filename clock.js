0f2854ab7a4bd35b1f8d3d27bce2387f815bed6e
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-23 11:14:27 -0700

    [js] fix working function for seconds

--- clock.js
+++ clock.js
@@ -1,9 +1,26 @@
-function secondRotation(secTick) {
-      let seconds = document.querySelector('#second');
-        seconds.rotate(10)
-       return seconds;
-        setInterval(10000};
-    }
+
+let seconds = document.querySelector('#second');
+let minutes = document.querySelector('#minute');
+let hours = document.querySelector('#hour');
+deg = 6;
+
+function secondRotation() {
+setInterval(function() {
+    
+    seconds.style.transform = 'rotate(' +deg + "deg)";
+    deg=(deg +6) % 360
+}, 1000);
+}
+secondRotation();
+
+
+
+// function secondRotation(secTick) {
+//       
+//         seconds.rotate(10)
+//        return seconds;
+//         setInterval(10000};
+//     }
 
     
     
