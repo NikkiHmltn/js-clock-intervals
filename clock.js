1e12dcb5b946d64583cb14ce164cee9b825e7164
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-23 09:23:22 -0700

    [js] add function for timer

---
+++ clock.js
@@ -0,0 +1,12 @@
+function secondRotation(secTick) {
+      let seconds = document.querySelector('#second');
+        seconds.rotate(10)
+       return seconds;
+        setInterval(10000};
+    }
+
+    
+    
+    
+    
+    
